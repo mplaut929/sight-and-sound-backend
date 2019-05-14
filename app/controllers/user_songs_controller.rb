5c85619c89cf786f03388a39643aa8c3a7e480be
github username <github email address>
2019-05-14 16:41:34 -0400

    made changes including adding serializers

--- app/controllers/user_songs_controller.rb
+++ app/controllers/user_songs_controller.rb
@@ -1,2 +1,6 @@
 class UserSongsController < ApplicationController
+  def index
+    @user_songs = UserSong.all
+    render json: @user_songs
+  end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,4 @@
 class Video < ApplicationRecord
+  has_many :video_songs
+  has_many :songs, through: :video_songs
 end
